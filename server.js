56b034928ee5004406a7a011c59a971796bb9933
ragog <giovanni@checklyhq.com>
2022-09-15 16:34:07 +0200

    fix: port and hostname for heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -16,6 +15,6 @@ const server = http.createServer((req, res) => {
   
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running on ${port}`);
 });
